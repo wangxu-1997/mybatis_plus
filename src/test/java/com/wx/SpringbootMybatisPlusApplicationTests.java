59c0ec6dec2eb49d5d42b938ec49b46b37602ad9
wangxu <1836679444@qq.com>
2021-03-27 22:00:28 +0800

    springboot初始化

---
+++ src/test/java/com/wx/SpringbootMybatisPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMybatisPlusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
